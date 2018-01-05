c621ecd2e31c6a4c2c09b204086ab48d14fccf51
James Harrison <JamesH@SAHL.COM>
2018-01-05 11:11:02 +0200

    PyramidSlideDown

---
+++ PyramidSlideDown/PyramidSlideDown.js
@@ -0,0 +1,15 @@
+function getLongestSlideDown(pyramid) {
+	for (let floor = pyramid.length - 1; floor > 0; floor--) //Moving bottom -> top
+	{
+		var parentFloorItems = pyramid[floor - 1];
+		var currentFloorItems = pyramid[floor];
+
+		for (let i = 0; i < pyramid[floor].length - 1; i++) //Moving left -> right
+		{
+			let itemOne = currentFloorItems[i];
+			let itemTwo = currentFloorItems[i + 1];
+			parentFloorItems[i] += Math.max(itemOne, itemTwo);
+		}
+	}
+	return pyramid[0][0];
+}
