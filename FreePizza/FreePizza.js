f133083e962a3b14aece4f684028c061a045fee4
James Harrison <James.Harrison.Za@gmail.com>
2018-01-07 18:58:00 +0200

    Added test

--- FreePizza/FreePizza.js
+++ FreePizza/FreePizza.js
@@ -7,3 +7,33 @@ function pizzaRewards(customers, minOrders, minPrice) {
 				.length >= minOrders;
 		});
 }
+
+// --------------------------------------------------------------------------------------------------------------
+// TESTS
+// --------------------------------------------------------------------------------------------------------------
+
+function areEqualArrays(arr1, arr2){
+
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+
+	for (let i=0; i < arr1.length; i++) {
+		if(arr1[i] !== arr2[i]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+const minOrders = 5
+const minPrice = 20
+const customers = {
+	'John Doe': [22, 30, 11, 17, 15, 52, 27, 12], // Only has three orders above 20$, so no pizza
+	'Jane Doe': [5, 17, 30, 33, 40, 22, 26, 10, 11, 45] // Has six orders above 20$, which means FREE PIZZA!
+}
+
+const expected = ['Jane Doe'];
+const actual = pizzaRewards(customers, minOrders, minPrice);
+
+console.log(areEqualArrays(expected, actual));
