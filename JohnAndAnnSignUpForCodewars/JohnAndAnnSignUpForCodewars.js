28ac4f1940eb4e35462a2e120b276f3dc4790cf4
James Harrison <JamesH@SAHL.COM>
2018-01-08 15:02:18 +0200

    Added tests

--- JohnAndAnnSignUpForCodewars/JohnAndAnnSignUpForCodewars.js
+++ JohnAndAnnSignUpForCodewars/JohnAndAnnSignUpForCodewars.js
@@ -18,3 +18,45 @@ function sumAnn(n) {
 // --------------------------------------------------------------------------------------------------------------
 // TESTS
 // --------------------------------------------------------------------------------------------------------------
+
+function areEqualArrays(arr1, arr2) {
+
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+
+	for (let i = 0; i < arr1.length; i++) {
+		if (arr1[i] !== arr2[i]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+(() => {
+	const expected = [0, 0, 1, 2, 2, 3, 4, 4, 5, 6, 6];
+	const actual = john(11);
+
+	console.log(`${desc}: ${areEqualArrays(actual, expected)}`);
+})('john');
+
+(() => {
+	const expected = [1, 1, 2, 2, 3, 3];
+	const actual = ann(6)
+
+	console.log(`${desc}: ${areEqualArrays(actual, expected)}`);
+})('ann');
+
+(() => {
+	const expected = 1720;
+	const actual = sumJohn(75)
+
+	console.log(`${desc}: ${areEqualArrays(actual, expected)}`);
+})('sumJohn');
+
+(() => {
+	const expected = 4070;
+	const actual = sumAnn(115)
+
+	console.log(`${desc}: ${areEqualArrays(actual, expected)}`);
+})('sumAnn');
