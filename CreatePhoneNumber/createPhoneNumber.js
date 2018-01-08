a3fced77c0f041aaed6c8f4dd842c79435b4efbb
James Harrison <JamesH@SAHL.COM>
2018-01-08 10:44:48 +0200

    Added another test

--- CreatePhoneNumber/createPhoneNumber.js
+++ CreatePhoneNumber/createPhoneNumber.js
@@ -6,7 +6,20 @@ function createPhoneNumber(numbers) {
 	return `(${numbers.slice(0, 3).join('')}) ${numbers.slice(3, 6).join('')}-${numbers.slice(6).join('')}`;
 }
 
-const expected = '(123) 456-7890';
-const actual = createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+// --------------------------------------------------------------------------------------------------------------
+// TESTS
+// --------------------------------------------------------------------------------------------------------------
 
-console.log(expected === actual, `expected: ${expected} to equal: ${actual}`);
+(() => {
+	const expected = '(123) 456-7890';
+	const actual = createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+
+	console.log(expected === actual, `expected: ${expected} to equal: ${actual}`);
+})();
+
+(() => {
+	const expected = '(042) 007-0007';
+	const actual = createPhoneNumber([0, 4, 2, 0, 0, 7, 0, 0, 0, 7]);
+
+	console.log(expected === actual, `expected: ${expected} to equal: ${actual}`);
+})();
