eed03851c178c6c35201dd2b7a87bf7f867dcbbb
James Harrison <JamesH@SAHL.COM>
2018-01-09 16:47:28 +0200

    Tests passing. Grouping array items with map

--- FindTheOddInt/FindTheOddInt.js
+++ FindTheOddInt/FindTheOddInt.js
@@ -1,11 +1,36 @@
-function findOdd(A) {
-	//happy coding!
-	return 0;
+'use strict';
+
+/** @param {Array<number>} arr */
+function findOdd(arr) {
+	
+	let oddInt;
+	const groupedItems = new Map();
+
+	for (const item of arr) {
+		if (!groupedItems.has(item)){
+			groupedItems.set(item, 0);
+		}
+		const prevValue = groupedItems.get(item);
+		groupedItems.set(item, prevValue + 1);
+	}
+
+	groupedItems.forEach((value, key, map) => {
+		if (value % 2 !== 0){
+			oddInt = key;
+		}
+	});
+
+	return oddInt;
 }
 
-// doTest([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5], 5);
-// doTest([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5], -1);
-// doTest([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5], 5);
-// doTest([10], 10);
-// doTest([1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1], 10);
-// doTest([5, 4, 3, 2, 1, 5, 4, 3, 2, 10, 10], 1);
+// --------------------------------------------------------------------------------------------------------------
+// TESTS
+// --------------------------------------------------------------------------------------------------------------
+
+console.log(findOdd([1, 1, 1]) === 1);
+console.log(findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]) === 5);
+console.log(findOdd([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5]) === -1);
+console.log(findOdd([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5]) === 5);
+console.log(findOdd([10]) === 10);
+console.log(findOdd([1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1]) === 10);
+console.log(findOdd([5, 4, 3, 2, 1, 5, 4, 3, 2, 10, 10]) === 1);
