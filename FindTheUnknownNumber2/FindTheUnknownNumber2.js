12b4c5af4c0a4410c165a65a5ed19edd60590750
James Harrison <JamesH@SAHL.COM>
2018-01-09 14:15:02 +0200

    Added tests

--- FindTheUnknownNumber2/FindTheUnknownNumber2.js
+++ FindTheUnknownNumber2/FindTheUnknownNumber2.js
@@ -1,4 +1,35 @@
 function findUnknowNumber(a, b, c, x, y, z) {
 
-	
 }
+
+// --------------------------------------------------------------------------------------------------------------
+// TESTS
+// --------------------------------------------------------------------------------------------------------------
+
+(() => {
+	const expected = 23;
+	const actual = findUnknowNumber(3, 5, 7, 2, 3, 2);
+
+	console.log(`${actual === expected}, expected: ${expected}, actual: ${actual}`);
+})();
+
+(() => {
+	const expected = 52;
+	const actual = findUnknowNumber(3, 5, 7, 1, 2, 3);
+
+	console.log(`${actual === expected}, expected: ${expected}, actual: ${actual}`);
+})();
+
+(() => {
+	const expected = 29;
+	const actual = findUnknowNumber(3, 7, 11, 2, 1, 7);
+
+	console.log(`${actual === expected}, expected: ${expected}, actual: ${actual}`);
+})();
+
+(() => {
+	const expected = 1;
+	const actual = findUnknowNumber(2, 3, 5, 1, 1, 1);
+
+	console.log(`${actual === expected}, expected: ${expected}, actual: ${actual}`);
+})();
