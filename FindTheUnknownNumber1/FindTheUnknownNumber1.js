0ca128881ff807becbbf421597eeb1551b10e98f
James Harrison <JamesH@SAHL.COM>
2018-01-09 11:36:32 +0200

    Added tests from codewars

--- FindTheUnknownNumber1/FindTheUnknownNumber1.js
+++ FindTheUnknownNumber1/FindTheUnknownNumber1.js
@@ -2,3 +2,13 @@ function findUnknowNumber(x, y, z) {
 	//coding and coding..
 
 }
+
+// --------------------------------------------------------------------------------------------------------------
+// TESTS
+// --------------------------------------------------------------------------------------------------------------
+
+console.log(findUnknowNumber(2, 3, 2) === 23);
+console.log(findUnknowNumber(1, 2, 3) === 52);
+console.log(findUnknowNumber(1, 3, 5) === 103);
+console.log(findUnknowNumber(0, 0, 0) === 105);
+console.log(findUnknowNumber(1, 1, 1) === 1);
