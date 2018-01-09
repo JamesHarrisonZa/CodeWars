1d1dbe3e4e473ce91dfc107736912ff2914efaa5
James Harrison <JamesH@SAHL.COM>
2018-01-09 12:26:23 +0200

    Tests passing! I made algorithm as efficient as possible. Other solutions were incrementing by 1 where I was trying to increment by a factor of x,y,z

--- FindTheUnknownNumber1/FindTheUnknownNumber1.js
+++ FindTheUnknownNumber1/FindTheUnknownNumber1.js
@@ -1,14 +1,61 @@
 function findUnknowNumber(x, y, z) {
-	//coding and coding..
 
+	let n = Math.max(x, y, z) > 0 ? Math.max(x, y, z): 1;
+
+	while (!(n % 7 === z && n % 5 === y && n % 3 === x)) {
+
+		if (n % 7 === z && z > 0) {
+			n += z;
+		}
+		else if (n % 5 === y && y > 0) {
+			n += y;
+		}
+		else if (n % 3 === x && x > 0) {
+			n += x;
+		}
+		else {
+			n++;
+		}
+	}
+	return n;
 }
 
 // --------------------------------------------------------------------------------------------------------------
 // TESTS
 // --------------------------------------------------------------------------------------------------------------
 
-console.log(findUnknowNumber(2, 3, 2) === 23);
-console.log(findUnknowNumber(1, 2, 3) === 52);
-console.log(findUnknowNumber(1, 3, 5) === 103);
-console.log(findUnknowNumber(0, 0, 0) === 105);
-console.log(findUnknowNumber(1, 1, 1) === 1);
+(() => {
+	const expected = 23;
+	const actual = findUnknowNumber(2, 3, 2);
+
+	console.log(`${actual === expected}, expected: ${expected}, actual: ${actual}`);
+})();
+
+(() => {
+	const expected = 52;
+	const actual = findUnknowNumber(1, 2, 3) ;
+
+	console.log(`${actual === expected}, expected: ${expected}, actual: ${actual}`);
+})();
+
+(() => {
+	const expected = 103;
+	const actual = findUnknowNumber(1, 3, 5);
+
+	console.log(`${actual === expected}, expected: ${expected}, actual: ${actual}`);
+})();
+
+(() => {
+	const expected = 105;
+	const actual = findUnknowNumber(0, 0, 0);
+
+	console.log(`${actual === expected}, expected: ${expected}, actual: ${actual}`);
+})();
+
+
+(() => {
+	const expected = 1;
+	const actual = findUnknowNumber(1, 1, 1);
+
+	console.log(`${actual === expected}, expected: ${expected}, actual: ${actual}`);
+})();
