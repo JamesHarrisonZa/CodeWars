650ac1f7214bc19fff73f9ca62b1229687e9f76e
JamesHarrisonZa <james.harrison.za@gmail.com>
2018-01-10 18:04:48 +0200

    Wip

--- GreedIsGood/GreedIsGood.js
+++ GreedIsGood/GreedIsGood.js
@@ -3,8 +3,38 @@
 /**
  * @param {Array<int>} dice 
  */
-function score(dice) {
-	// Fill me in!
+function score(diceRolls) {
+
+	let score = 0;
+	const diceRollCounter = new Array(7).fill(0);
+
+	for (const roll of diceRolls) {
+		diceRollCounter[roll] ++;
+	}
+	
+	for (let diceNumber = 1; diceNumber <= diceRollCounter.length; diceNumber++) {
+
+		if (diceRollCounter[diceNumber] === 0){
+			continue;
+		}
+
+		if (diceRollCounter[diceNumber] % 3 === 0) {
+			let diceScore = (diceRollCounter[diceNumber] / 3) * diceNumber * 100;
+			if (diceNumber === 1){
+				diceScore = diceScore*10;
+			}
+			score += diceScore;
+		}
+		else {
+			if (diceNumber === 1) {
+				score += (diceRollCounter[diceNumber] % 3) * 100;
+			}
+			if (diceNumber === 5) {
+				score += (diceRollCounter[diceNumber] % 3) * 50;
+			}
+		}
+	}
+	return score;
 }
 
 // --------------------------------------------------------------------------------------------------------------
@@ -31,3 +61,9 @@ function score(dice) {
 
 	console.log(`${actual === expected}, expected: ${expected}, actual: ${actual}`);
 })();
+
+// [3, 3, 3, 3, 3] === 300
+
+// [1, 1, 1, 1, 3] === 1100
+
+// [1, 1, 1, 1, 5] === 1150
