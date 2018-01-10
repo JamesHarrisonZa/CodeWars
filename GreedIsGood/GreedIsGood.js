6ac020d20eaba17d7ba7f2c8092c9ac5de933b9d
JamesHarrisonZa <james.harrison.za@gmail.com>
2018-01-10 17:30:26 +0200

    Added tests !

--- GreedIsGood/GreedIsGood.js
+++ GreedIsGood/GreedIsGood.js
@@ -10,3 +10,24 @@ function score(dice) {
 // --------------------------------------------------------------------------------------------------------------
 // TESTS
 // --------------------------------------------------------------------------------------------------------------
+
+(() => {
+	const expected = 0;
+	const actual = score([2, 3, 4, 6, 2]);
+
+	console.log(`${actual === expected}, expected: ${expected}, actual: ${actual}`);
+})();
+
+(() => {
+	const expected = 400;
+	const actual = score([4, 4, 4, 3, 3]);
+
+	console.log(`${actual === expected}, expected: ${expected}, actual: ${actual}`);
+})();
+
+(() => {
+	const expected = 450;
+	const actual = score([2, 4, 4, 5, 4]);
+
+	console.log(`${actual === expected}, expected: ${expected}, actual: ${actual}`);
+})();
