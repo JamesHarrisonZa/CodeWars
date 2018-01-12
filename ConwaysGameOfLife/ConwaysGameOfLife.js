442e04366688cd93b48a01491a27bd41b20a5d19
JamesHarrisonZa <james.harrison.za@gmail.com>
2018-01-12 12:13:17 +0200

    Improved test output

--- ConwaysGameOfLife/ConwaysGameOfLife.js
+++ ConwaysGameOfLife/ConwaysGameOfLife.js
@@ -56,7 +56,7 @@ function areEqualArrays(arr1, arr2){
 		];
 	const actual = nextGen(startingCells)
 
-	console.log(`${desc}: ${areEqualArrays(actual, expected)}`);
+	console.log(`${areEqualArrays(expected, actual)}. ${desc}. expected: ${expected}, actual: ${actual}`);
 })('Given a 3x3 grid with all dead cells the next generation should remain all dead');
 
 ((desc) => {
@@ -74,7 +74,7 @@ function areEqualArrays(arr1, arr2){
 		];
 	const actual = nextGen(startingCells)
 
-	console.log(`${desc}: ${areEqualArrays(actual, expected)}`);
+	console.log(`${areEqualArrays(expected, actual)}. ${desc}. expected: ${expected}, actual: ${actual}`);
 })('Given a 3x3 grid with a single live cell the next generation should kill the lonely cell');
 
 ((desc) => {
@@ -92,7 +92,7 @@ function areEqualArrays(arr1, arr2){
 		];
 	const actual = nextGen(startingCells)
 
-	console.log(`${desc}: ${areEqualArrays(actual, expected)}`);
+	console.log(`${areEqualArrays(expected, actual)}. ${desc}. expected: ${expected}, actual: ${actual}`);
 })('Given a 3x3 grid with two dead cells with three live neighbours the next generation should bring two cells to life');
 
 ((desc) => {
@@ -110,7 +110,7 @@ function areEqualArrays(arr1, arr2){
 		];
 	const actual = nextGen(startingCells)
 
-	console.log(`${desc}: ${areEqualArrays(actual, expected)}`);
+	console.log(`${areEqualArrays(expected, actual)}. ${desc}. expected: ${expected}, actual: ${actual}`);
 })('Given a 3x3 grid with a live cell with two live neighbours the next generation should keep that cell alive');
 
 ((desc) => {
@@ -128,7 +128,7 @@ function areEqualArrays(arr1, arr2){
 		];
 	const actual = nextGen(startingCells)
 
-	console.log(`${desc}: ${areEqualArrays(actual, expected)}`);
+	console.log(`${areEqualArrays(expected, actual)}. ${desc}. expected: ${expected}, actual: ${actual}`);
 })('Given a 3x3 grid with a live cell with more than three neighbours the next generation should kill that cell due to overcrowding');
 
 ((desc) => {
@@ -146,5 +146,5 @@ function areEqualArrays(arr1, arr2){
 		];
 	const actual = nextGen(startingCells)
 
-	console.log(`${desc}: ${areEqualArrays(actual, expected)}`);
+	console.log(`${areEqualArrays(expected, actual)}. ${desc}. expected: ${expected}, actual: ${actual}`);
 })('Given a 3x3 grid with all live cells the next generation should have a cross of dead cells');
