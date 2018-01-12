6602a1f232e596304a3ca87aa49a9c9aa57cbbc0
JamesHarrisonZa <james.harrison.za@gmail.com>
2018-01-12 12:13:31 +0200

    Improved test output

--- FindTheUnknownNumber1/FindTheUnknownNumber1.js
+++ FindTheUnknownNumber1/FindTheUnknownNumber1.js
@@ -11,38 +11,38 @@ function findUnknowNumber(x, y, z) {
 // TESTS
 // --------------------------------------------------------------------------------------------------------------
 
-(() => {
+((desc) => {
 	const expected = 23;
 	const actual = findUnknowNumber(2, 3, 2);
 
-	console.log(`${actual === expected}, expected: ${expected}, actual: ${actual}`);
-})();
+	console.log(`${actual === expected}. ${desc}. expected: ${expected}, actual: ${actual}`);
+})('Test 1');
 
-(() => {
+((desc) => {
 	const expected = 52;
 	const actual = findUnknowNumber(1, 2, 3) ;
 
-	console.log(`${actual === expected}, expected: ${expected}, actual: ${actual}`);
-})();
+	console.log(`${actual === expected}. ${desc}. expected: ${expected}, actual: ${actual}`);
+})('Test 2');
 
-(() => {
+((desc) => {
 	const expected = 103;
 	const actual = findUnknowNumber(1, 3, 5);
 
-	console.log(`${actual === expected}, expected: ${expected}, actual: ${actual}`);
-})();
+	console.log(`${actual === expected}. ${desc}. expected: ${expected}, actual: ${actual}`);
+})('Test 3');
 
-(() => {
+((desc) => {
 	const expected = 105;
 	const actual = findUnknowNumber(0, 0, 0);
 
-	console.log(`${actual === expected}, expected: ${expected}, actual: ${actual}`);
-})();
+	console.log(`${actual === expected}. ${desc}. expected: ${expected}, actual: ${actual}`);
+})('Test 4');
 
 
-(() => {
+((desc) => {
 	const expected = 1;
 	const actual = findUnknowNumber(1, 1, 1);
 
-	console.log(`${actual === expected}, expected: ${expected}, actual: ${actual}`);
-})();
+	console.log(`${actual === expected}. ${desc}. expected: ${expected}, actual: ${actual}`);
+})('Test 5');
