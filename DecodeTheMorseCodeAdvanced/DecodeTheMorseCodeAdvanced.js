210566491a4344008435047e58dc88c23c2b1fcb
JamesHarrisonZa <james.harrison.za@gmail.com>
2018-01-12 12:13:26 +0200

    Improved test output

--- DecodeTheMorseCodeAdvanced/DecodeTheMorseCodeAdvanced.js
+++ DecodeTheMorseCodeAdvanced/DecodeTheMorseCodeAdvanced.js
@@ -111,7 +111,9 @@ MORSE_CODE['--..'] = 'Z';
 // TESTS
 // --------------------------------------------------------------------------------------------------------------
 
-const expected = 'HEY JUDE';
-const actual = decodeMorse(decodeBits('1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011'));
+((desc) => {
+	const expected = 'HEY JUDE';
+	const actual = decodeMorse(decodeBits('1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011'));
 
-console.log(expected === actual);
+	console.log(`${actual === expected}. ${desc}. expected: ${expected}, actual: ${actual}`);
+})('should decode binary to alphabet');
