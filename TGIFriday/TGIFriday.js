b8d4ccb33aedba3f4c3ccd22462370989c0778ae
JamesHarrisonZa <james.harrison.za@gmail.com>
2018-01-12 12:14:03 +0200

    Improved test output

--- TGIFriday/TGIFriday.js
+++ TGIFriday/TGIFriday.js
@@ -36,26 +36,26 @@ function lastDayIsFriday(initialYear, endYear) {
 	const expected = 2;
 	const actual = lastDayIsFriday(2017)
 
-	console.log(`${desc}: ${expected === actual}`);
+	console.log(`${actual === expected}. ${desc}. expected: ${expected}, actual: ${actual}`);
 })('returns 2 when 2017');
 
 ((desc) => {
 	const expected = 1;
 	const actual = lastDayIsFriday(1991)
 
-	console.log(`${desc}: ${expected === actual}`);
+	console.log(`${actual === expected}. ${desc}. expected: ${expected}, actual: ${actual}`);
 })('returns 1 when 1991');
 
 ((desc) => {
 	const expected = 171;
 	const actual = lastDayIsFriday(1901, 2000)
 
-	console.log(`${desc}: ${expected === actual}`);
+	console.log(`${actual === expected}. ${desc}. expected: ${expected}, actual: ${actual}`);
 })('returns 171 when between 1901 and 2000');
 
 ((desc) => {
 	const expected = 200;
 	const actual = lastDayIsFriday(1901, 2017)
 
-	console.log(`${desc}: ${expected === actual}`);
+	console.log(`${actual === expected}. ${desc}. expected: ${expected}, actual: ${actual}`);
 })('returns 200 when between 1991 and 2017');
