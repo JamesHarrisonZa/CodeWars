f2165957338557e8f76b8bd8af46c08a8f5aee31
JamesHarrisonZa <james.harrison.za@gmail.com>
2018-01-15 17:09:49 +0200

    started on nextBiggerNumber

---
+++ NextBiggerNumber/nextBiggerNumber.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n 
+ */
+function nextBigger(n) {
+	//your code here
+}
+
+// --------------------------------------------------------------------------------------------------------------
+// TESTS
+// --------------------------------------------------------------------------------------------------------------
+
+console.log(nextBigger(12) === 21)
+console.log(nextBigger(513) === 531)
+console.log(nextBigger(2017) === 2071)
+console.log(nextBigger(414) === 441)
+console.log(nextBigger(144) === 414)
