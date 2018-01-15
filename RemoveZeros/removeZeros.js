81030ba27b86c28d070e9764fe40bca1261bf644
JamesHarrisonZa <james.harrison.za@gmail.com>
2018-01-15 12:54:31 +0200

    refactored and added some failing tests

--- RemoveZeros/removeZeros.js
+++ RemoveZeros/removeZeros.js
@@ -3,21 +3,24 @@
  */
 function removeZeros(array) {
 
+	const needsSwitching = (i) => (array[i] === 0 || array[i] === "0");
+
 	for (let i = 0; i < array.length; i++) {
-		if(array[i] !== 0){
+		if (!needsSwitching(i)) {
 			continue;
 		}
 
 		let j;
 		for (j = i + 1; j < array.length; j++) {
-			if(array[j] !== 0) {
+			if (!needsSwitching(j)) {
 				if (array[j] === undefined) return array;
 				else {
+					const zeroNumberOrString = array[i];
 					array[i] = array[j];
-					array[j] = 0;
+					array[j] = zeroNumberOrString;
 					break;
 				}
-			}			
+			}
 		}
 	}
 	return array;
@@ -41,10 +44,26 @@ function areEqualArrays(arr1, arr2) {
 	return true;
 }
 
+// ((desc) => {
+
+// 	const expected = [7, 2, 3, 4, 6, 13, 78, 19, 14, 0, 0, 0, 0, 0, 0];
+// 	const actual = removeZeros([7, 2, 3, 0, 4, 6, 0, 0, 13, 0, 78, 0, 0, 19, 14]);
+
+// 	console.log(`${areEqualArrays(expected, actual)}. ${desc}. expected: ${expected}, actual: ${actual}`);
+// })('Given zeros');
+
 ((desc) => {
 
-	const expected = [7, 2, 3, 4, 6, 13, 78, 19, 14, 0, 0, 0, 0, 0, 0];
-	const actual = removeZeros([7, 2, 3, 0, 4, 6, 0, 0, 13, 0, 78, 0, 0, 19, 14]);
+	const expected = [1, "0", "0", "0", 0];
+	const actual = removeZeros(['0', '0', '0', 0, 1]); //1,null,"5","0","2",8,6,null,false,0
 
 	console.log(`${areEqualArrays(expected, actual)}. ${desc}. expected: ${expected}, actual: ${actual}`);
-})('');
+})('Given mixed types 2');
+
+// ((desc) => {
+
+// 	const expected = [1, , "5", "2", 8, 6, , false, "0", 0];
+// 	const actual = removeZeros([1, null , 5, 0, 2, 0, 8, 6, null , false]); //1,null,"5","0","2",8,6,null,false,0
+
+// 	console.log(`${areEqualArrays(expected, actual)}. ${desc}. expected: ${expected}, actual: ${actual}`);
+// })('Given mixed types with boolean');
