6e606615298176ef2150c000a0ea3e536be93da1
JamesHarrisonZa <james.harrison.za@gmail.com>
2018-01-15 12:10:45 +0200

    Test passing

--- RemoveZeros/removeZeros.js
+++ RemoveZeros/removeZeros.js
@@ -2,16 +2,24 @@
  * @param {Array<number>} array 
  */
 function removeZeros(array) {
-	// Sort "array" so that all elements with the value of zero are moved to the
-	// end of the array, while the other elements maintain order.
-	// [0, 1, 2, 0, 3] --> [1, 2, 3, 0, 0]
-	// Zero elements also maintain order in which they occurred.
-	// [0, "0", 1, 2, 3] --> [1, 2, 3, 0, "0"]
 
-	// Do not use any temporary arrays or objects. Additionally, you're not able
-	// to use any Array or Object prototype methods such as .shift(), .push(), etc
+	for (let i = 0; i < array.length; i++) {
+		if(array[i] !== 0){
+			continue;
+		}
 
-	// the correctly sorted array should be returned.
+		let j;
+		for (j = i + 1; j < array.length; j++) {
+			if(array[j] !== 0) {
+				if (array[j] === undefined) return array;
+				else {
+					array[i] = array[j];
+					array[j] = 0;
+					break;
+				}
+			}			
+		}
+	}
 	return array;
 }
 
