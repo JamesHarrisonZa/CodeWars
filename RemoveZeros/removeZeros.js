cb8ab90f3d3de9af0c92133b79597e916bfe9e5c
JamesHarrisonZa <james.harrison.za@gmail.com>
2018-01-15 15:59:34 +0200

    changed design

--- RemoveZeros/removeZeros.js
+++ RemoveZeros/removeZeros.js
@@ -3,23 +3,16 @@
  */
 function removeZeros(array) {
 
-	const needsSwitching = (i) => (array[i] === 0 || array[i] === "0");
+	const needsSwitching = (i) => (array[i] === 0 || array[i] === '0');
 
-	for (let i = 0; i < array.length; i++) {
-		if (!needsSwitching(i)) {
-			continue;
-		}
-
-		let j;
-		for (j = i + 1; j < array.length; j++) {
-			if (!needsSwitching(j)) {
-				if (array[j] === undefined) return array;
-				else {
-					const zeroNumberOrString = array[i];
-					array[i] = array[j];
-					array[j] = zeroNumberOrString;
-					break;
-				}
+	for (var i = array.length - 1; i >= 0; i--) {
+		if (needsSwitching(i)) {
+			j = i;
+			while ((j + 1 < array.length) && !needsSwitching(j + 1)) {
+				const zeroNumberOrString = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = zeroNumberOrString;
+				j++;
 			}
 		}
 	}
@@ -44,26 +37,18 @@ function areEqualArrays(arr1, arr2) {
 	return true;
 }
 
-// ((desc) => {
+((desc) => {
 
-// 	const expected = [7, 2, 3, 4, 6, 13, 78, 19, 14, 0, 0, 0, 0, 0, 0];
-// 	const actual = removeZeros([7, 2, 3, 0, 4, 6, 0, 0, 13, 0, 78, 0, 0, 19, 14]);
+	const expected = [7, 2, 3, 4, 6, 13, 78, 19, 14, 0, 0, 0, 0, 0, 0];
+	const actual = removeZeros([7, 2, 3, 0, 4, 6, 0, 0, 13, 0, 78, 0, 0, 19, 14]);
 
-// 	console.log(`${areEqualArrays(expected, actual)}. ${desc}. expected: ${expected}, actual: ${actual}`);
-// })('Given zeros');
+	console.log(`${areEqualArrays(expected, actual)}. ${desc}. expected: ${expected}, actual: ${actual}`);
+})('Given zeros');
 
 ((desc) => {
 
-	const expected = [1, "0", "0", "0", 0];
-	const actual = removeZeros(['0', '0', '0', 0, 1]); //1,null,"5","0","2",8,6,null,false,0
+	const expected = [1, '0', '0', '0', 0];
+	const actual = removeZeros(['0', '0', '0', 0, 1]);
 
 	console.log(`${areEqualArrays(expected, actual)}. ${desc}. expected: ${expected}, actual: ${actual}`);
-})('Given mixed types 2');
-
-// ((desc) => {
-
-// 	const expected = [1, , "5", "2", 8, 6, , false, "0", 0];
-// 	const actual = removeZeros([1, null , 5, 0, 2, 0, 8, 6, null , false]); //1,null,"5","0","2",8,6,null,false,0
-
-// 	console.log(`${areEqualArrays(expected, actual)}. ${desc}. expected: ${expected}, actual: ${actual}`);
-// })('Given mixed types with boolean');
+})('Given mixed types');
