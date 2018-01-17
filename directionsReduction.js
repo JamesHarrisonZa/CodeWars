9f9388b8afd90ad1ee62db01a08119b94a5289c8
JamesHarrisonZa <james.harrison.za@gmail.com>
2018-01-17 12:23:48 +0200

    refactored and all tests passing

--- directionsReduction.js
+++ directionsReduction.js
@@ -1,8 +1,32 @@
 // https://www.codewars.com/kata/directions-reduction
 
+const opposites = {
+	NORTH: 'SOUTH',
+	SOUTH: 'NORTH',
+	EAST: 'WEST',
+	WEST: 'EAST'
+};
+
+/**
+ * @param {Array} arr 
+ */
 function dirReduc(arr) {
 	
+	arr.map((d, i) => {
+		if (isNextToOpposite(arr, i)) {
+			arr[i] = null;
+			arr[i + 1] = null;
+			i++;
+		};
+	});
+
+	if (wasReduced(arr)) return dirReduc(arr.filter(x => x));
+	return arr;
 }
+
+const isNextToOpposite = (arr, i) => arr[i + 1] === opposites[arr[i]];
+
+const wasReduced = (arr) => arr.filter(x => !x).length > 0;
 
 // --------------------------------------------------------------------------------------------------------------
 // TESTS
@@ -19,22 +43,22 @@ function areEqualArrays(arr1, arr2) {
 }
 
 ((desc) => {
-	const expected = ["WEST"];
-	const actual = dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]);
+	const expected = ['WEST'];
+	const actual = dirReduc(['NORTH', 'SOUTH', 'SOUTH', 'EAST', 'WEST', 'NORTH', 'WEST']);
 
 	console.log(`${areEqualArrays(expected, actual)}. ${desc}. expected: ${expected}, actual: ${actual}`);
 })('Test 1');
 
 ((desc) => {
-	const expected = ["NORTH", "WEST", "SOUTH", "EAST"];
-	const actual = dirReduc(["NORTH", "WEST", "SOUTH", "EAST"]);
+	const expected = ['NORTH', 'WEST', 'SOUTH', 'EAST'];
+	const actual = dirReduc(['NORTH', 'WEST', 'SOUTH', 'EAST']);
 
 	console.log(`${areEqualArrays(expected, actual)}. ${desc}. expected: ${expected}, actual: ${actual}`);
 })('Test 2');
 
 ((desc) => {
 	const expected = [];
-	const actual = dirReduc(["NORTH", "SOUTH", "EAST", "WEST", "EAST", "WEST"]);
+	const actual = dirReduc(['NORTH', 'SOUTH', 'EAST', 'WEST', 'EAST', 'WEST']);
 
 	console.log(`${areEqualArrays(expected, actual)}. ${desc}. expected: ${expected}, actual: ${actual}`);
 })('Test 3');
