8e87c2077145c2005c961fe4fc8627cc5cfc67e6
JamesHarrisonZa <james.harrison.za@gmail.com>
2018-01-17 09:57:05 +0200

    All tests passing

--- DeepComparisonOfObjects.js
+++ DeepComparisonOfObjects.js
@@ -5,10 +5,14 @@
  * @param {Object} obj2 
  */
 function deepCompare(obj1, obj2) {
+
+	if (typeof obj1 !== 'object' || (obj1 === null || obj1 === undefined)) return obj1 === obj2;
+	if (Object.keys(obj1).length !== Object.keys(obj2).length) return false;
+
 	return Object.keys(obj1)
 		.every(key => {
-			if (typeof obj1[key] === "object") return deepCompare(obj1[key], obj2[key]);
-			return obj1[key] === obj2[key]
+			if (typeof obj1[key] === 'object') return deepCompare(obj1[key], obj2[key]);
+			return (obj1[key] === obj2[key]) && (typeof obj1[key] === typeof obj2[key])
 		});
 };
 
@@ -50,3 +54,67 @@ function deepCompare(obj1, obj2) {
 
 	console.log(`${actual === expected}. ${desc}. expected: ${expected}, actual: ${actual}`);
 })('Different array property');
+
+((desc) => {
+	const expected = false;
+	const actual = deepCompare(1, '1');
+
+	console.log(`${actual === expected}. ${desc}. expected: ${expected}, actual: ${actual}`);
+})('string vs int');
+
+((desc) => {
+	const expected = true;
+	const actual = deepCompare(1, 1);
+
+	console.log(`${actual === expected}. ${desc}. expected: ${expected}, actual: ${actual}`);
+})('same int vs int');
+
+((desc) => {
+	const expected = false;
+	const actual = deepCompare(1, 2);
+
+	console.log(`${actual === expected}. ${desc}. expected: ${expected}, actual: ${actual}`);
+})('Different int vs int');
+
+((desc) => {
+	const expected = false;
+	const actual = deepCompare(null, undefined);
+
+	console.log(`${actual === expected}. ${desc}. expected: ${expected}, actual: ${actual}`);
+})('null vs undefined');
+
+((desc) => {
+	const expected = false;
+	const actual = deepCompare(null, undefined);
+
+	console.log(`${actual === expected}. ${desc}. expected: ${expected}, actual: ${actual}`);
+})('undefined vs null');
+
+((desc) => {
+	const expected = false;
+	const actual = deepCompare(null, undefined);
+
+	console.log(`${actual === expected}. ${desc}. expected: ${expected}, actual: ${actual}`);
+})('int vs null');
+
+((desc) => {
+	const expected = false;
+	const actual = deepCompare(null, undefined);
+
+	console.log(`${actual === expected}. ${desc}. expected: ${expected}, actual: ${actual}`);
+})('null vs int');
+
+
+((desc) => {
+	const expected = true;
+	const actual = deepCompare({ name: 'Joe', surname: 'Smith' }, { name: 'Joe', surname: 'Smith' });
+
+	console.log(`${actual === expected}. ${desc}. expected: ${expected}, actual: ${actual}`);
+})('Same obj vs obj');
+
+((desc) => {
+	const expected = false;
+	const actual = deepCompare({ name: 'Joe' }, { name: 'Joe', surname: 'Smith' });
+
+	console.log(`${actual === expected}. ${desc}. expected: ${expected}, actual: ${actual}`);
+})('Missing property');
