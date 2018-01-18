2f4973f4343b279075a3d95df3f4756811106f10
JamesHarrisonZa <james.harrison.za@gmail.com>
2018-01-18 16:34:42 +0200

    Refactoring

--- conwaysGameOfLife.js
+++ conwaysGameOfLife.js
@@ -28,16 +28,11 @@ function nextGen(cells) {
 
 function areEqualArrays(arr1, arr2){
 
-	if (arr1.length !== arr2.length) {
-		return false;
-	}
+	if (arr1.length !== arr2.length) return false;
 
 	for (let i=0; i < arr1.length; i++) {
 		for (let j=0; j < arr1[i].length; j++) {
-			
-			if(arr1[i][j] !== arr2[i][j]) {
-				return false;
-			}
+			if (arr1[i][j] !== arr2[i][j]) return false;
 		}
 	}
 	return true;
