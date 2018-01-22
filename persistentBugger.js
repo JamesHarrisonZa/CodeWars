be4a5c7f5b4491469d7f30e4ee35e52131ba5431
JamesHarrisonZa <james.harrison.za@gmail.com>
2018-01-22 15:39:28 +0200

    Started on persistentBugger

---
+++ persistentBugger.js
@@ -0,0 +1,37 @@
+// https://www.codewars.com/kata/persistent-bugger/train/javascript
+
+const persistence = (num) => {
+	//code me
+}
+
+// --------------------------------------------------------------------------------------------------------------
+// TESTS
+// --------------------------------------------------------------------------------------------------------------
+
+((desc) => {
+	const expected = 0;
+	const actual = persistence(4);
+
+	console.log(`${actual === expected}. ${desc}. expected: ${expected}, actual: ${actual}`);
+})('persistence(4) === 0');
+
+((desc) => {
+	const expected = 2;
+	const actual = persistence(25);
+
+	console.log(`${actual === expected}. ${desc}. expected: ${expected}, actual: ${actual}`);
+})('persistence(25) === 2');
+
+((desc) => {
+	const expected = 3;
+	const actual = persistence(39);
+
+	console.log(`${actual === expected}. ${desc}. expected: ${expected}, actual: ${actual}`);
+})('persistence(39) === 3');
+
+((desc) => {
+	const expected = 4;
+	const actual = persistence(999);
+
+	console.log(`${actual === expected}. ${desc}. expected: ${expected}, actual: ${actual}`);
+})('persistence(999) === 4');
