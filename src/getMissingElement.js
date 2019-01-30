bbe114c4eb5095c3fb4e1bce406cd8df47968cbf
JamesHarrisonZa <james.harrison.za@gmail.com>
2019-01-30 09:55:13 +0200

    Using Object.values() rather than Object.keys() so I dont have to do a type conversion.

--- src/getMissingElement.js
+++ src/getMissingElement.js
@@ -3,7 +3,7 @@
 function getMissingElement(superImportantArray) {
 
 	// (O)2n Solution ? 
-
+	
 	// const numbersHashMap = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].reduce((hashMap, number) => {
 	// 	hashMap[number] = number;
 	// 	return hashMap;
@@ -13,8 +13,9 @@ function getMissingElement(superImportantArray) {
 	superImportantArray.forEach(number => {
 		delete numbersHashMap[number];
 	});
-	const filteredArray = Object.keys(numbersHashMap); // (O)2n + 1?
-	return filteredArray[0]/1; //Weird I have to convert from string to number here?
+	const filteredArray = Object.values(numbersHashMap); // (O)2n + 1?
+
+	return filteredArray[0];
 }
 
 // --------------------------------------------------------------------------------------------------------------
@@ -26,11 +27,11 @@ function getMissingElement(superImportantArray) {
 	const actual = getMissingElement([0, 5, 1, 3, 2, 9, 7, 6, 4]);
 
 	console.log(`${actual === expected}. ${desc}. expected: ${expected}, actual: ${actual}`);
-})("getMissingElement([0, 5, 1, 3, 2, 9, 7, 6, 4]) === 8");
+})('getMissingElement([0, 5, 1, 3, 2, 9, 7, 6, 4]) === 8');
 
 ((desc) => {
 	const expected = 3;
 	const actual = getMissingElement([9, 2, 4, 5, 7, 0, 8, 6, 1]);
 
 	console.log(`${actual === expected}. ${desc}. expected: ${expected}, actual: ${actual}`);
-})("getMissingElement( [9,2,4,5,7,0,8,6,1]) === 3");
+})('getMissingElement( [9,2,4,5,7,0,8,6,1]) === 3');
