24cdef609aace2c0e6c2df4df6d70523a843c560
JamesHarrisonZa <james.harrison.za@gmail.com>
2019-01-30 10:56:10 +0200

    Initial implementation after specs are in place

--- src/creditCardChecker.js
+++ src/creditCardChecker.js
@@ -1,9 +1,36 @@
 //https://www.codewars.com/kata/credit-card-issuer-checking
 
 
-function getIssuer(cardNumber) {
+function getIssuer(number) {
 
-	return '??';
+	const cardNumber = new String(number);
+
+	if (cardNumber.startsWith('34') || cardNumber.startsWith('37')){
+
+		if (cardNumber.length === 15)
+			return 'AMEX';
+		return 'Unknown';
+	}
+
+	if (cardNumber.startsWith('6011')) {
+		if (cardNumber.length === 16)
+			return 'Discover';
+		return 'Unknown';
+	}
+
+	if (cardNumber.startsWith('51') || cardNumber.startsWith('52') || cardNumber.startsWith('53') || cardNumber.startsWith('54') || cardNumber.startsWith('55')){
+		if (cardNumber.length === 16)
+			return 'Mastercard';
+		return 'Unknown';
+	}
+
+	if (cardNumber.startsWith('4')) {
+		if (cardNumber.length === 13 || cardNumber.length === 16)
+			return 'VISA';
+		return 'Unknown';
+	}
+
+	return 'Unknown';
 };
 
 module.exports = getIssuer;
--- src/creditCardChecker.spec.js
+++ src/creditCardChecker.spec.js
@@ -3,15 +3,180 @@
 const assert = require('assert');
 const getIssuer = require('./creditCardChecker');
 
-describe('Given card number', () => {
+describe.only('Given card number', () => {
 
-	describe('that starts with 4', () => {
+	describe('that starts with', () => {
 
-		it('should return VISA', () => {
-			const cardNumber = 4111111111111111;
-			const actual = getIssuer(cardNumber);
-			const expected = 'VISA';
-			assert.equal(actual, expected);
+		describe('34 ', () => {
+
+			describe('and length of 15', () => {
+			
+				it('should return AMEX', () => {
+					const cardNumber = 340000000000000;
+					const actual = getIssuer(cardNumber);
+					const expected = 'AMEX';
+					assert.equal(actual, expected);
+				});
+			});
+			describe('and length not of 15', () => {
+			
+				it('should return Unknown', () => {
+					const cardNumber = 340;
+					const actual = getIssuer(cardNumber);
+					const expected = 'Unknown';
+					assert.equal(actual, expected);
+				});
+			});
+		});
+
+		describe('37', () => {
+
+			describe('and length of 15', () => {
+
+				it('should return AMEX', () => {
+					const cardNumber = 370000000000000;
+					const actual = getIssuer(cardNumber);
+					const expected = 'AMEX';
+					assert.equal(actual, expected);
+				});
+			});
+			describe('and length not of 15', () => {
+
+				it('should return Unknown', () => {
+					const cardNumber = 370;
+					const actual = getIssuer(cardNumber);
+					const expected = 'Unknown';
+					assert.equal(actual, expected);
+				});
+			});
+		});
+
+		describe('6011', () => {
+
+			describe('and length of 16', () => {
+
+				it('should return Discover', () => {
+					const cardNumber = 6011000000000000;
+					const actual = getIssuer(cardNumber);
+					const expected = 'Discover';
+					assert.equal(actual, expected);
+				});
+			 });
+
+			describe('and length not of 16', () => {
+
+				it('should return Unknown', () => {
+					const cardNumber = 60110;
+					const actual = getIssuer(cardNumber);
+					const expected = 'Unknown';
+					assert.equal(actual, expected);
+				});
+			});
+		});
+
+		describe('51', () => {
+
+			describe('and length of 16', () => {
+
+				it('should return Mastercard', () => {
+					const cardNumber = 5100000000000000;
+					const actual = getIssuer(cardNumber);
+					const expected = 'Mastercard';
+					assert.equal(actual, expected);
+				});
+			});
+
+			describe('and length not of 16', () => {
+
+				it('should return Unknown', () => {
+					const cardNumber = 510;
+					const actual = getIssuer(cardNumber);
+					const expected = 'Unknown';
+					assert.equal(actual, expected);
+				});
+			});
+		});
+
+		describe('52', () => {
+
+			it('should return Mastercard', () => {
+				const cardNumber = 5200000000000000;
+				const actual = getIssuer(cardNumber);
+				const expected = 'Mastercard';
+				assert.equal(actual, expected);
+			});
+		});
+
+		describe('53', () => {
+
+			it('should return Mastercard', () => {
+				const cardNumber = 5300000000000000;
+				const actual = getIssuer(cardNumber);
+				const expected = 'Mastercard';
+				assert.equal(actual, expected);
+			});
+		});
+
+		describe('54', () => {
+
+			it('should return Mastercard', () => {
+				const cardNumber = 5400000000000000;
+				const actual = getIssuer(cardNumber);
+				const expected = 'Mastercard';
+				assert.equal(actual, expected);
+			});
+		});
+
+		describe('55', () => {
+
+			it('should return Mastercard', () => {
+				const cardNumber = 5500000000000000;
+				const actual = getIssuer(cardNumber);
+				const expected = 'Mastercard';
+				assert.equal(actual, expected);
+			});
+		});
+
+		describe('4', () => {
+
+			describe('and length of 13', () => {
+
+				it('should return VISA', () => {
+					const cardNumber = 4000000000000;
+					const actual = getIssuer(cardNumber);
+					const expected = 'VISA';
+					assert.equal(actual, expected);
+				});
+			});
+
+			describe('and length of 16', () => {
+
+				it('should return VISA', () => {
+					const cardNumber = 4000000000000000;
+					const actual = getIssuer(cardNumber);
+					const expected = 'VISA';
+					assert.equal(actual, expected);
+				});
+			});
+
+			describe('and length not of 16', () => {
+
+				it('should return Unknown', () => {
+					const cardNumber = 40;
+					const actual = getIssuer(cardNumber);
+					const expected = 'Unknown';
+					assert.equal(actual, expected);
+				});
+			});
+		});
+		describe('something else', () => {
+			
+			it('should return Unknown', () => {
+				const cardNumber = 900000000000000;
+				const actual = getIssuer(cardNumber);
+				const expected = 'Unknown';
+				assert.equal(actual, expected);
+			});
 		});
 	});
 });
