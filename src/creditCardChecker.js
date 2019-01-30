85154e0d3b6e9af7b87953775d440fcfd36c134d
JamesHarrisonZa <james.harrison.za@gmail.com>
2019-01-30 16:09:32 +0200

    New design once specs are in place

--- src/creditCardChecker.js
+++ src/creditCardChecker.js
@@ -1,36 +1,37 @@
 //https://www.codewars.com/kata/credit-card-issuer-checking
 
 
-function getIssuer(number) {
+var cardTypes = [
+	{ 
+		type: "AMEX", 
+		prefix: ["34", "37"], 
+		length: [15] 
+	},
+	{ 
+		type: "Discover", 
+		prefix: ["6011"], 
+		length: [16] 
+	},
+	{ 
+		type: "Mastercard", 
+		prefix: ["51", "52", "53", "54", "55"], 
+		length: [16] },
+	{ 
+		type: "VISA", 
+		prefix: ["4"], 
+		length: [13, 16] 
+	},
+];
+
+function getIssuer(number){
 
 	const cardNumber = new String(number);
 
-	if (cardNumber.startsWith('34') || cardNumber.startsWith('37')){
-
-		if (cardNumber.length === 15)
-			return 'AMEX';
-		return 'Unknown';
-	}
-
-	if (cardNumber.startsWith('6011')) {
-		if (cardNumber.length === 16)
-			return 'Discover';
-		return 'Unknown';
-	}
-
-	if (cardNumber.startsWith('51') || cardNumber.startsWith('52') || cardNumber.startsWith('53') || cardNumber.startsWith('54') || cardNumber.startsWith('55')){
-		if (cardNumber.length === 16)
-			return 'Mastercard';
-		return 'Unknown';
-	}
-
-	if (cardNumber.startsWith('4')) {
-		if (cardNumber.length === 13 || cardNumber.length === 16)
-			return 'VISA';
-		return 'Unknown';
-	}
-
-	return 'Unknown';
-};
+	for (var i in cardTypes)
+		if (cardTypes[i].prefix.some(p => { return cardNumber.startsWith(p) }) &&
+			cardTypes[i].length.some(l => { return cardNumber.length == l }))
+			return cardTypes[i].type
+	return "Unknown"
+}
 
 module.exports = getIssuer;
