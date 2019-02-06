edd31dad49699577ef549c3018d6672d68e3dc7d
JamesHarrisonZa <james.harrison.za@gmail.com>
2019-02-06 15:05:34 +0200

    Specs passing

--- src/arrayDeepCount.js
+++ src/arrayDeepCount.js
@@ -1,8 +1,26 @@
 'use strict'
 
-function deepCount() {
+/**
+* @param {Array<any>} array
+*/
+function deepCount(array) {
 
-	return 42;
+	let count = array.length;
+
+	count += countInnerArrays(array);
+
+	return count;
+}
+
+function countInnerArrays(array) {
+	let innerCount = 0;
+	for (const item of array) {
+		if (Array.isArray(item)) {
+			innerCount += countInnerArrays(item);
+			innerCount += item.length;
+		}
+	}
+	return innerCount;
 }
 
 module.exports = deepCount;
--- src/arrayDeepCount.spec.js
+++ src/arrayDeepCount.spec.js
@@ -3,9 +3,50 @@ const deepCount = require('./arrayDeepCount');
 
 describe.only('deepCount', () => {
 
-	it('should return 42', () => {
-		const actual = deepCount();
-		const expected = 42;
-		assert.equal(actual, expected);
+	describe('given []', () => {
+
+		it('should return 0', () => {
+			const actual = deepCount([]);
+			const expected = 0;
+			assert.equal(actual, expected);
+		});
 	});
+
+	describe('given [1, 2, 3]', () => {
+	
+		it('should return 0', () => {
+			const actual = deepCount([1, 2, 3]);
+			const expected = 3;
+			assert.equal(actual, expected);
+		});
+	});
+
+	describe('given ["x", "y", ["z"]]', () => {
+	
+		it('should return 4', () => {
+			const actual = deepCount(["x", "y", ["z"]]);
+			const expected = 4;
+			assert.equal(actual, expected);
+		});
+	});
+
+	describe('given [1, 2, [3, 4, [5]]]', () => {
+	
+		it('should return 7', () => {
+			const actual = deepCount([1, 2, [3, 4, [5]]]);
+			const expected = 7;
+			assert.equal(actual, expected);
+		});
+	});
+
+	describe('given [[[[[[[[[]]]]]]]]]', () => {
+	
+		it('should return 8', () => {
+			const actual = deepCount([[[[[[[[[]]]]]]]]]);
+			const expected = 8;
+			assert.equal(actual, expected);
+		});
+	});
+
+
 });
