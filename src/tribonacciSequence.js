df0d1a310948cd1381844082cc7c23164767168b
James Harrison <James.Harrison.Za@gmail.com>
2019-02-12 19:40:59 +0200

    Solution done. Yay!

--- src/tribonacciSequence.js
+++ src/tribonacciSequence.js
@@ -1,9 +1,21 @@
+//https://www.codewars.com/kata/tribonacci-sequence
 
+/**
+ * @param {Array<number>} signature 
+ * @param {number} n 
+ */
 function tribonacci(signature, n) {
-	
-	if(n === 0) {
-		return [];
+
+	if (n < 3) {
+		return signature.slice(0, n);
 	}
+
+	const sequence = signature.concat(new Array(n-3));
+
+	for (let i = 3; i < n; i++) {
+		sequence[i] = sequence[i - 3] + sequence[i - 2] + sequence[i - 1];
+	}
+	return sequence;
 }
 
 module.exports = tribonacci;
--- src/tribonacciSequence.spec.js
+++ src/tribonacciSequence.spec.js
@@ -12,7 +12,7 @@ describe.only('tribonacci', () => {
 		});
 	});
 
-	describe.skip('given signature [1, 1, 1] and n === 10', () => {
+	describe('given signature [1, 1, 1] and n === 10', () => {
 	
 		it('should return [1, 1, 1, 3, 5, 9, 17, 31, 57, 105]', () => {
 			const actual = tribonacci([1, 1, 1], 10);
@@ -21,7 +21,7 @@ describe.only('tribonacci', () => {
 		});
 	});
 
-	describe.skip('given signature [0, 0, 1] and n === 10', () => {
+	describe('given signature [0, 0, 1] and n === 10', () => {
 	
 		it('should return', () => {
 			const actual = [0, 0, 1, 1, 2, 4, 7, 13, 24, 44];
@@ -30,7 +30,7 @@ describe.only('tribonacci', () => {
 		});
 	});
 
-	describe.skip('given signature [0, 1, 1] and n === 10', () => {
+	describe('given signature [0, 1, 1] and n === 10', () => {
 
 		it('should return', () => {
 			const actual = [0, 1, 1, 2, 4, 7, 13, 24, 44, 81];
@@ -39,7 +39,7 @@ describe.only('tribonacci', () => {
 		});
 	});
 
-	describe.skip('given signature [1, 0, 0] and n === 10', () => {
+	describe('given signature [1, 0, 0] and n === 10', () => {
 
 		it('should return', () => {
 			const actual = [1, 0, 0, 1, 1, 2, 4, 7, 13, 24];
@@ -48,7 +48,7 @@ describe.only('tribonacci', () => {
 		});
 	});
 
-	describe.skip('given signature [0, 0, 0] and n === 10', () => {
+	describe('given signature [0, 0, 0] and n === 10', () => {
 
 		it('should return', () => {
 			const actual = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
@@ -57,7 +57,7 @@ describe.only('tribonacci', () => {
 		});
 	});
 
-	describe.skip('given signature [1, 2, 3] and n === 10', () => {
+	describe('given signature [1, 2, 3] and n === 10', () => {
 
 		it('should return', () => {
 			const actual = [1, 2, 3, 6, 11, 20, 37, 68, 125, 230];
@@ -66,7 +66,7 @@ describe.only('tribonacci', () => {
 		});
 	});
 
-	describe.skip('given signature [3, 2, 1] and n === 10', () => {
+	describe('given signature [3, 2, 1] and n === 10', () => {
 
 		it('should return', () => {
 			const actual = [3, 2, 1, 6, 9, 16, 31, 56, 103, 190];
@@ -75,7 +75,7 @@ describe.only('tribonacci', () => {
 		});
 	});
 
-	describe.skip('given signature [1, 1, 1] and n === 1', () => {
+	describe('given signature [1, 1, 1] and n === 1', () => {
 
 		it('should return', () => {
 			const actual = [1];
@@ -84,7 +84,7 @@ describe.only('tribonacci', () => {
 		});
 	});
 
-	describe.skip('given signature [300, 200, 100] and n === 0', () => {
+	describe('given signature [300, 200, 100] and n === 0', () => {
 
 		it('should return', () => {
 			const actual = [];
@@ -93,7 +93,7 @@ describe.only('tribonacci', () => {
 		});
 	});
 
-	describe.skip('given signature [0.5, 0.5, 0.5] and n === 30', () => {
+	describe('given signature [0.5, 0.5, 0.5] and n === 30', () => {
 
 		it('should return', () => {
 			const actual = [0.5, 0.5, 0.5, 1.5, 2.5, 4.5, 8.5, 15.5, 28.5, 52.5, 96.5, 177.5, 326.5, 600.5, 1104.5, 2031.5, 3736.5, 6872.5, 12640.5, 23249.5, 42762.5, 78652.5, 144664.5, 266079.5, 489396.5, 900140.5, 1655616.5, 3045153.5, 5600910.5, 10301680.5];
