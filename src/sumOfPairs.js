5123b3d1598cf346fb859092c577307da5cefcf1
James Harrison <James.Harrison.Za@gmail.com>
2019-02-13 19:23:39 +0200

    Unsubmitted solution. //ToDo, optimize. Test with 10000 items is timing out.

--- src/sumOfPairs.js
+++ src/sumOfPairs.js
@@ -1,7 +1,37 @@
 //https://www.codewars.com/kata/sum-of-pairs/train/javascript
 
-var sum_pairs = function (ints, s) {
-	//your code here
+//ToDo, optimize. Test with 10000 items is timing out.
+
+/**
+ * 
+ * @param {Array<number>} numbers 
+ * @param {number} sum 
+ */
+var sum_pairs = function (numbers, sum) {
+
+	let currentPair;
+	let currentPairEnd;
+
+	for (let i = 0; i < numbers.length; i++) {
+		
+		if (currentPairEnd !== undefined && i > currentPairEnd){
+			return currentPair;
+		}
+
+		let first = numbers[i];
+		for (let j = i + 1; j < numbers.length; j++) {
+			let second = numbers[j];
+			
+			if (first + second === sum) {
+
+				if (currentPairEnd === undefined || j < currentPairEnd) {
+					currentPair = [first, second];
+					currentPairEnd = j;
+				}
+			}
+		}
+	}
+	return currentPair;
 }
 
 module.exports = sum_pairs;
--- src/sumOfPairs.spec.js
+++ src/sumOfPairs.spec.js
@@ -14,4 +14,28 @@ describe('sumOfPairs', () => {
 			});
 		});
 	});
+
+	describe('given [1, -2, 3, 0, -6, 1]', () => {
+
+		describe('and sum of -6', () => {
+
+			it('should return [0, -6]', () => {
+				const actual = sumOfPairs([1, -2, 3, 0, -6, 1], -6);
+				const expected = [0, -6];
+				assert.deepEqual(actual, expected);
+			});
+		});
+	});
+
+	describe('given [10,5,2,3,7,5]', () => {
+	
+		describe('and sum of 10', () => {
+		
+			it('should return [3, 7]', () => {
+				const expected = sumOfPairs([10, 5, 2, 3, 7, 5], 10);
+				const actual = [3, 7];
+				assert.deepEqual(actual, expected);
+			});
+		});
+	});
 });
