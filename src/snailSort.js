1a645371f1d7d76148b2b60ba6c3abd02a22bfd3
JamesHarrisonZa <james.harrison.za@gmail.com>
2019-03-08 16:29:36 +0200

    working for 3x3

--- src/snailSort.js
+++ src/snailSort.js
@@ -7,33 +7,37 @@ function snail (array) {
 	
 	const demension = array.length;
 	const sortedArray = new Array();
-	//console.log('length: ' + sortedArray.length);
 
-	//leftTraversal
-	for (let leftIndex = 0; leftIndex < demension; leftIndex++) {
-		sortedArray.push(array[0][leftIndex]);
+	for (let circle = 0; circle < demension - 1; circle++) {
+
+		console.log(`circle: ${circle}`)
+
+		//rightTraversal
+		for (let leftIndex = circle; leftIndex < demension - circle; leftIndex++) {
+			sortedArray.push(array[circle][leftIndex]);
+		}
+		console.log(`After rightTraversal,  sortedArray: ${sortedArray}`);
+
+		//downTraversal
+		for (let downIndex = circle + 1; downIndex < demension - circle; downIndex++) {
+			sortedArray.push(array[downIndex][demension - 1]);
+		}
+		console.log(`After downTraversal,  sortedArray: ${sortedArray}`);
+
+
+		//leftTraversal
+		for (let rightIndex = demension - 2 - circle; rightIndex >= circle; rightIndex--) { //< -- Adding 8 again for circle 2
+			//console.log(`${rightIndex}: rightIndex`);
+			sortedArray.push(array[demension - 1][rightIndex]);
+		}
+		console.log(`After leftTraversal, sortedArray: ${sortedArray}`);
+
+		//upTraversal
+		for (let upIndex = demension - 1 - 1; upIndex >= circle + 1; upIndex--) {
+			sortedArray.push(array[upIndex][circle]);
+		}
+		console.log(`After upTraversal,    sortedArray: ${sortedArray}`);
 	}
-	console.log(`After leftTraversal, sortedArray: ${sortedArray}`);
-
-	//downTraversal
-	for (let downIndex = 1; downIndex < demension; downIndex++) {
-		sortedArray.push(array[downIndex][demension-1]);
-	}
-	console.log(`After downTraversal, sortedArray: ${sortedArray}`);
-
-
-	//rightTraversal
-	for (let rightIndex = demension - 1 - 1; rightIndex >= 0; rightIndex--) {
-		sortedArray.push(array[demension - 1][rightIndex]);
-	}
-	console.log(`After rightTraversal, sortedArray: ${sortedArray}`);
-
-	//upTraversal
-	for (let upIndex = demension- 1; upIndex < demension - 1; upIndex--) {
-		console.log(`upIndex: ${upIndex}`);
-		sortedArray.push(array[upIndex][0]);
-	}
-	console.log(`After upTraversal, sortedArray: ${sortedArray}`);
 	return sortedArray;
 }
 
--- src/snailSort.spec.js
+++ src/snailSort.spec.js
@@ -1,14 +1,16 @@
 const assert = require('assert');
 const snail = require('./snailSort');
 
-describe('snailSort', () => {
+describe.only('snailSort', () => {
 
-	describe.only('given 2x2 array', () => {
+	describe('given 2x2 array', () => {
 
-		const array = [[1, 2],
-					   [3, 4]]
+		const array = [
+			[1, 2],
+			[3, 4]
+		];
 
-		it('should toDo', () => {
+		it('should return [1, 2, 4, 3]', () => {
 			const actual = snail(array);
 			const expected = [1, 2, 4, 3];
 			assert.deepEqual(actual, expected);
@@ -17,14 +19,32 @@ describe('snailSort', () => {
 
 	describe('given 3x3 array', () => {
 	
-	const array = [[1, 2, 3],
-				   [4, 5, 6],
-				   [7, 8, 9]]
+		const array = [
+			[1, 2, 3],
+			[4, 5, 6],
+			[7, 8, 9]
+		];
 
-		it('should toDo', () => {
+		it('should return [1, 2, 3, 6, 9, 8, 7, 4, 5]', () => {
 			const actual = snail(array);
 			const expected = [1, 2, 3, 6, 9, 8, 7, 4, 5];
-			assert.equal(actual, expected);
+			assert.deepEqual(actual, expected);
 		});
 	});
+
+	// describe('given a 4x4 array', () => {
+
+	// 	const array = [
+	// 		[01, 02, 03, 04],
+	// 		[05, 06, 07, 08],
+	// 		[09, 10, 11, 12],
+	// 		[13, 14, 15, 16]
+	// 	];
+
+	// 	it('should return ', () => {
+	// 		const actual = snail(array);
+	// 		const expected = [1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10];
+	// 		assert.deepEqual(actual, expected);
+	// 	});
+	// });
 });
