7bfe43aa20aaec8bf83c9ae27066cb17e1440e8a
JamesHarrisonZa <james.harrison.za@gmail.com>
2019-03-26 15:29:36 +0200

    Solution accepted on codewars

--- src/humanReadableTime.js
+++ src/humanReadableTime.js
@@ -1,9 +1,23 @@
 //https://www.codewars.com/kata/human-readable-time/train/javascript
 'use strict'
 
-function humanReadable(seconds) {
+function humanReadable(totalSeconds) {
 
-	return '00:00:00';
+	const toalMinutes = Math.floor(totalSeconds / 60);
+	
+	const hours = Math.floor(toalMinutes / 60);
+	const minutes = toalMinutes - (hours * 60);
+	const seconds = totalSeconds - (toalMinutes * 60);
+
+	return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)}`;
+}
+
+function padZero(number){
+
+	if (number < 10){
+		return `0${number}`
+	}
+	return number;
 }
 
 module.exports = humanReadable;
--- src/humanReadableTime.spec.js
+++ src/humanReadableTime.spec.js
@@ -3,11 +3,47 @@ const humanReadable  = require('./humanReadableTime');
 
 describe.only('humanReadable', () => {
 
-	describe('given 0', () => {
+	describe('given 0 seconds', () => {
 	
 		it('should return 00:00:00', () => {
 			const actual = humanReadable(0);
 			const expected = '00:00:00';
+			assert.equal(actual, expected);
+		});
+	});
+
+	describe('given 5 seconds', () => {
+	
+		it('should return 00:00:05', () => {
+			const actual = humanReadable(5);
+			const expected = '00:00:05';
+			assert.equal(actual, expected);
+		});
+	});
+
+	describe('given 60 seconds', () => {
+
+		it('should return 00:01:00', () => {
+			const actual = humanReadable(60);
+			const expected = '00:01:00';
+			assert.equal(actual, expected);
+		});
+	});
+
+	describe('given 86399 seconds', () => {
+
+		it('should return 23:59:59', () => {
+			const actual = humanReadable(86399);
+			const expected = '23:59:59';
+			assert.equal(actual, expected);
+		});
+	});
+
+	describe('given 359999 seconds', () => {
+
+		it('should return 99:59:59', () => {
+			const actual = humanReadable(359999);
+			const expected = '99:59:59';
 			assert.equal(actual, expected);
 		});
 	});
