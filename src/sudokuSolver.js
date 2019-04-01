4227fa0acc385d292cc1aee8b6f0c21750af29fa
JamesHarrisonZa <james.harrison.za@gmail.com>
2019-04-01 11:21:17 +0200

    Started

---
+++ src/sudokuSolver.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/5296bc77afba8baa690002d7
+
+'use strict'
+
+function sudoku() {
+
+	return 42;
+}
+
+module.exports = sudoku;
---
+++ src/sudokuSolver.spec.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const sudoku = require('./sudokuSolver');
+
+describe.skip('sudokuSolver', () => {
+
+	describe('given ', () => {
+	
+		it('should toDo', () => {
+			const actual = sudoku();
+			const expected = 42;
+			assert.equal(actual, expected);
+		});
+	});
+});
