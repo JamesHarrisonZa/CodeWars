a31fd556a194328e9a6f4adcdffc112721ab8cb1
JamesHarrisonZa <james.harrison.za@gmail.com>
2019-04-01 16:23:28 +0200

    GetUpdatedRow

--- src/sudokuSolver.js
+++ src/sudokuSolver.js
@@ -2,9 +2,35 @@
 
 'use strict'
 
-function sudoku() {
+const _idealRow = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-	return 42;
+/**
+ * @param {Array<Array<number>>} grid 
+ */
+function sudoku(grid) {
+
+	//Check horizontal rows
+	for (let i = 0; i < grid.length; i++) {
+
+		const updatedRow = GetUpdatedRow(grid[i]);
+		grid[i] = updatedRow;
+	}
+
+	return grid;
+}
+
+/**
+ * @param {Array<number>} row 
+ */
+function GetUpdatedRow(row) {
+
+	const missingNumbers = _idealRow.filter(x => !row.includes(x));
+
+	if (missingNumbers.length === 1) {
+		const indexToReplace = row.indexOf(0);
+		row[indexToReplace] = missingNumbers[0];
+	}
+	return row;
 }
 
 module.exports = sudoku;
--- src/sudokuSolver.spec.js
+++ src/sudokuSolver.spec.js
@@ -1,13 +1,46 @@
 const assert = require('assert');
 const sudoku = require('./sudokuSolver');
 
-describe.skip('sudokuSolver', () => {
+describe.only('sudokuSolver', () => {
 
-	describe('given ', () => {
+	describe('given grid with 1 row and 1 missing number', () => {
 	
-		it('should toDo', () => {
-			const actual = sudoku();
-			const expected = 42;
+		const puzzle = [[1,2,3,4,5,6,7,8,0]];
+
+		it('should return filled row', () => {
+			const actual = sudoku(puzzle);
+			const expected = [[1, 2, 3, 4, 5, 6, 7, 8, 9]];
+			assert.deepEqual(actual, expected);
+		});
+	});
+
+	describe.skip('given puzzle', () => {
+	
+		const puzzle = [
+			[5, 3, 0, 0, 7, 0, 0, 0, 0],
+			[6, 0, 0, 1, 9, 5, 0, 0, 0],
+			[0, 9, 8, 0, 0, 0, 0, 6, 0],
+			[8, 0, 0, 0, 6, 0, 0, 0, 3],
+			[4, 0, 0, 8, 0, 3, 0, 0, 1],
+			[7, 0, 0, 0, 2, 0, 0, 0, 6],
+			[0, 6, 0, 0, 0, 0, 2, 8, 0],
+			[0, 0, 0, 4, 1, 9, 0, 0, 5],
+			[0, 0, 0, 0, 8, 0, 0, 7, 9]
+		];
+
+		it('should solve it', () => {
+			const actual = sudoku(puzzle);
+			const expected = [
+				[5, 3, 4, 6, 7, 8, 9, 1, 2],
+				[6, 7, 2, 1, 9, 5, 3, 4, 8],
+				[1, 9, 8, 3, 4, 2, 5, 6, 7],
+				[8, 5, 9, 7, 6, 1, 4, 2, 3],
+				[4, 2, 6, 8, 5, 3, 7, 9, 1],
+				[7, 1, 3, 9, 2, 4, 8, 5, 6],
+				[9, 6, 1, 5, 3, 7, 2, 8, 4],
+				[2, 8, 7, 4, 1, 9, 6, 3, 5],
+				[3, 4, 5, 2, 8, 6, 1, 7, 9]
+			];
 			assert.equal(actual, expected);
 		});
 	});
