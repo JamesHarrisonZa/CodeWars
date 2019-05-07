80f6522abe78f0845e6fd800649183bd0f44d181
JamesHarrisonZa <james.harrison.za@gmail.com>
2019-05-07 16:25:19 +0200

    Solution working

--- src/playingWithDigits.js
+++ src/playingWithDigits.js
@@ -1,8 +1,27 @@
 //https://www.codewars.com/kata/5552101f47fc5178b1000050
 
+/**
+ * @param {number} n 
+ * @param {number} p 
+ */
 function digPow(n, p) {
 	
-	return 1;
+	const digits = n
+		.toString()
+		.split('')
+		.map(x => Number(x));
+
+	const powerSum = digits
+		.map(x => x ** p++)
+		.reduce((prev, curr) => prev + curr, 0);
+
+	let k = 1;
+	while (n * k <= powerSum) {
+		if (n * k === powerSum)
+			return k;
+		k++;
+	}
+	return -1;
 }
 
 module.exports = digPow;
--- src/playingWithDigits.spec.js
+++ src/playingWithDigits.spec.js
@@ -4,10 +4,17 @@ const assert = require('assert');
 describe('digPow', () => {
 
 	describe('89 --> 8¹ + 9² = 89 * 1', () => {
-	
 		it('should return 1', () => {
 			const actual = digPow(89, 1);
 			const expected = 1;
+			assert.equal(actual, expected);
+		});
+	});
+
+	describe('695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2', () => {
+		it('should return 2', () => {
+			const actual = digPow(695, 2);
+			const expected = 2;
 			assert.equal(actual, expected);
 		});
 	});
